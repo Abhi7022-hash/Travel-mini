c76267872ca3727917d9966c390833e41731073b
abhim7022 <abhishekdindavar049@gmail.com>
2025-08-18 19:34:52 +0530

    Update test-flight.py

--- services/flights/tests/test-flight.py
+++ services/flights/tests/test-flight.py
@@ -1,1 +1,17 @@
+import requests
+from multiprocessing import Process
+import time
+import app as flights_app
 
+def run_app():
+    flights_app.app.run(port=5006)
+
+def test_list_flights():
+    p = Process(target=run_app)
+    p.start()
+    time.sleep(1)
+    res = requests.get('http://localhost:5006/api/flights')
+    assert res.status_code == 200
+    data = res.json()
+    assert isinstance(data, list)
+    p.terminate()
