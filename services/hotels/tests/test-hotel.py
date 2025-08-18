01f06921b5ed043cc6091691a9d73f2ae65e6a55
abhim7022 <abhishekdindavar049@gmail.com>
2025-08-18 19:36:09 +0530

    Update test-hotel.py

--- services/hotels/tests/test-hotel.py
+++ services/hotels/tests/test-hotel.py
@@ -1,1 +1,18 @@
 
+import requests
+from multiprocessing import Process
+import time
+import app as hotels_app
+
+def run_app():
+    hotels_app.app.run(port=5007)
+
+def test_list_hotels():
+    p = Process(target=run_app)
+    p.start()
+    time.sleep(1)
+    res = requests.get('http://localhost:5007/api/hotels')
+    assert res.status_code == 200
+    data = res.json()
+    assert isinstance(data, list)
+    p.terminate()
