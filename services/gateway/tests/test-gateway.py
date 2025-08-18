1d6d9b1f8a4e255f5288cf81d6a4edef873642be
abhim7022 <abhishekdindavar049@gmail.com>
2025-08-18 19:33:30 +0530

    Update test-gateway.py

--- services/gateway/tests/test-gateway.py
+++ services/gateway/tests/test-gateway.py
@@ -1,1 +1,15 @@
+import requests
+from multiprocessing import Process
+import time
+import app as gateway_app
 
+def run_app():
+    gateway_app.app.run(port=5005)
+
+def test_healthz():
+    p = Process(target=run_app)
+    p.start()
+    time.sleep(1)
+    res = requests.get('http://localhost:5005/healthz')
+    assert res.status_code == 200
+    p.terminate()
